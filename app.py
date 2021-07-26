483eadd5f7a2f9fff770f1977e3457427460591e
Ali Hassan <73987102+dooomdev@users.noreply.github.com>
2021-07-26 14:05:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store_db.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store_db.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 Bootstrap(app)
